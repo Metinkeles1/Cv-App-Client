8ce2776b502eaf6c18195979a88a985673736675
Metin Keleş <the.metin9@gmail.com>
2023-04-07 12:22:32 +0300

    Design

--- src/app/admin/components/educations/educations.module.ts
+++ src/app/admin/components/educations/educations.module.ts
@@ -14,6 +14,7 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DeleteDialogComponent } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { FileUploadModule } from 'src/app/services/common/file-upload/file-upload.module';
+import { DialogModule } from 'src/app/dialogs/dialog.module';
 
 @NgModule({
   declarations: [
@@ -21,21 +22,16 @@ import { FileUploadModule } from 'src/app/services/common/file-upload/file-uploa
     CreateComponent,
     ListComponent,
     DeleteDirective,
-    DeleteDialogComponent
+    // DeleteDialogComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', component: EducationsComponent }
     ]),
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatDialogModule,
-    FileUploadModule
+    MatSidenavModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule, MatPaginatorModule,
+    DialogModule,
+    FileUploadModule,
   ]
 })
 export class EducationsModule { }
